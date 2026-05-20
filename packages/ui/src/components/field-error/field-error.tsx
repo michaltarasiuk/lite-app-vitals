59b469fe60b432cb129c4af1f6872444f82f0467
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-05-20 14:02:23 +0200

    add field error component

---
+++ packages/ui/src/components/field-error/field-error.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import type { FieldErrorVariants } from "@lite-app-vitals/styles/components/field-error";
+import { fieldErrorVariants } from "@lite-app-vitals/styles/components/field-error";
+import type { ComponentPropsWithRef } from "react";
+import { FieldError as FieldErrorPrimitive } from "react-aria-components/FieldError";
+
+import { composeTwRenderProps } from "../../utils/compose";
+
+/*
+ * Field Error Root
+ */
+interface FieldErrorRootProps
+  extends
+    ComponentPropsWithRef<typeof FieldErrorPrimitive>,
+    FieldErrorVariants {}
+
+const FieldErrorRoot = ({
+  children,
+  className,
+  ...rest
+}: FieldErrorRootProps) => (
+  <FieldErrorPrimitive
+    data-visible
+    className={composeTwRenderProps(className, fieldErrorVariants())}
+    data-slot="field-error"
+    {...rest}
+  >
+    {(renderProps) =>
+      typeof children === "function" ? children(renderProps) : children
+    }
+  </FieldErrorPrimitive>
+);
+
+/*
+ * Exports
+ */
+export { FieldErrorRoot };
+
+export type { FieldErrorRootProps };
---
+++ packages/ui/src/components/field-error/index.ts
@@ -0,0 +1,32 @@
+import type { ComponentProps } from "react";
+
+import { FieldErrorRoot } from "./field-error";
+
+/*
+ * Compound Component
+ */
+export const FieldError = Object.assign(FieldErrorRoot, {
+  Root: FieldErrorRoot,
+});
+
+export interface FieldError {
+  Props: ComponentProps<typeof FieldErrorRoot>;
+  RootProps: ComponentProps<typeof FieldErrorRoot>;
+}
+
+/*
+ * Named Component
+ */
+export { FieldErrorRoot };
+
+export type {
+  FieldErrorRootProps as FieldErrorProps,
+  FieldErrorRootProps,
+} from "./field-error";
+
+/*
+ * Variants
+ */
+export { fieldErrorVariants } from "@lite-app-vitals/styles/components/field-error";
+
+export type { FieldErrorVariants } from "@lite-app-vitals/styles/components/field-error";
