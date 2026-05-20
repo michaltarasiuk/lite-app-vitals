31b77365d8d1b80b00cae6d927f1c4a9a8281b78
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-05-20 11:52:55 +0200

    add alias file

---
+++ packages/ui/src/components/textfield/index.ts
@@ -0,0 +1,37 @@
+import type { ComponentProps } from "react";
+
+import { TextFieldRoot } from "./textfield";
+
+/*
+ * Compound Component
+ */
+export const TextField = Object.assign(TextFieldRoot, {
+  Root: TextFieldRoot,
+});
+
+export interface TextField {
+  Props: ComponentProps<typeof TextFieldRoot>;
+  RootProps: ComponentProps<typeof TextFieldRoot>;
+}
+
+/*
+ * Named Component
+ */
+export { TextFieldRoot };
+
+export type {
+  TextFieldRootProps as TextFieldProps,
+  TextFieldRootProps,
+} from "./textfield";
+
+/*
+ * Context
+ */
+export { TextFieldContext } from "./textfield";
+
+/*
+ * Variants
+ */
+export { textFieldVariants } from "@lite-app-vitals/styles/components/textfield";
+
+export type { TextFieldVariants } from "@lite-app-vitals/styles/components/textfield";
