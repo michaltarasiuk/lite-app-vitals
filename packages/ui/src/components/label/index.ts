300d92a002ee259da74328171e5060737cfa3dad
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-05-20 14:20:51 +0200

    fix label alias

--- packages/ui/src/components/label/index.ts
+++ packages/ui/src/components/label/index.ts
@@ -1,1 +1,29 @@
-export * from "./label";
+import type { ComponentProps } from "react";
+
+import { LabelRoot } from "./label";
+
+/*
+ * Compound Component
+ */
+export const Label = Object.assign(LabelRoot, {
+  Root: LabelRoot,
+});
+
+export interface Label {
+  Props: ComponentProps<typeof LabelRoot>;
+  RootProps: ComponentProps<typeof LabelRoot>;
+}
+
+/*
+ * Named Component
+ */
+export { LabelRoot };
+
+export type { LabelRootProps as LabelProps, LabelRootProps } from "./label";
+
+/*
+ * Variants
+ */
+export { labelVariants } from "@lite-app-vitals/styles/components/label";
+
+export type { LabelVariants } from "@lite-app-vitals/styles/components/label";
