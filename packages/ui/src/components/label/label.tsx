e11a5a1dd993865f2b521c3b578820ce8fb24b5b
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-05-20 12:07:42 +0200

    add label component

---
+++ packages/ui/src/components/label/label.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import type { LabelVariants } from "@lite-app-vitals/styles/components/label";
+import { labelVariants } from "@lite-app-vitals/styles/components/label";
+import type { ComponentPropsWithRef } from "react";
+import { Label as LabelPrimitive } from "react-aria-components/Label";
+
+/*
+ * Label Root
+ */
+interface LabelRootProps
+  extends ComponentPropsWithRef<typeof LabelPrimitive>, LabelVariants {}
+
+const LabelRoot = ({
+  children,
+  className,
+  isDisabled,
+  isInvalid,
+  isRequired,
+  ...rest
+}: LabelRootProps) => (
+  <LabelPrimitive
+    className={labelVariants({ className, isDisabled, isInvalid, isRequired })}
+    data-slot="label"
+    {...rest}
+  >
+    {children}
+  </LabelPrimitive>
+);
+
+/*
+ * Exports
+ */
+export { LabelRoot };
+
+export type { LabelRootProps };
