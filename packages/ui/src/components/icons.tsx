1058be2d0f3ae0b12e4d8a1eb03ca1fd95c65ec3
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-05-20 14:20:39 +0200

    add link component

--- packages/ui/src/components/icons.tsx
+++ packages/ui/src/components/icons.tsx
@@ -22,3 +22,26 @@ export const IconChevronDown = (props: IconProps) => (
     />
   </svg>
 );
+
+export const ExternalLinkIcon = ({
+  height = 9,
+  width = 9,
+  ...props
+}: IconProps) => (
+  <svg
+    aria-hidden="true"
+    aria-label="External link icon"
+    fill="none"
+    height={height}
+    role="presentation"
+    viewBox="0 0 7 7"
+    width={width}
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M1.20592 6.84333L0.379822 6.01723L4.52594 1.8672H1.37819L1.38601 0.731812H6.48742V5.83714H5.34421L5.35203 2.6933L1.20592 6.84333Z"
+      fill="currentColor"
+    />
+  </svg>
+);
---
+++ packages/ui/src/components/link/index.ts
@@ -0,0 +1,35 @@
+import type { ComponentProps } from "react";
+
+import { LinkIcon, LinkRoot } from "./link";
+
+/*
+ * Compound Component
+ */
+export const Link = Object.assign(LinkRoot, {
+  Icon: LinkIcon,
+  Root: LinkRoot,
+});
+
+export interface Link {
+  Props: ComponentProps<typeof LinkRoot>;
+  RootProps: ComponentProps<typeof LinkRoot>;
+  IconProps: ComponentProps<typeof LinkIcon>;
+}
+
+/*
+ * Named Component
+ */
+export { LinkIcon, LinkRoot };
+
+export type {
+  LinkIconProps,
+  LinkRootProps as LinkProps,
+  LinkRootProps,
+} from "./link";
+
+/*
+ * Variants
+ */
+export { linkVariants } from "@lite-app-vitals/styles/components/link";
+
+export type { LinkVariants } from "@lite-app-vitals/styles/components/link";
