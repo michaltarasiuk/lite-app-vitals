8647261a35e85475457d99c1f45791190eb65ab7
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-05-20 11:49:48 +0200

    add textfield component

---
+++ packages/ui/src/components/textfield/textfield.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import type { TextFieldVariants } from "@lite-app-vitals/styles/components/textfield";
+import { textFieldVariants } from "@lite-app-vitals/styles/components/textfield";
+import type { ComponentPropsWithRef } from "react";
+import React, { createContext } from "react";
+import { TextField as TextFieldPrimitive } from "react-aria-components/TextField";
+
+import { composeTwRenderProps } from "../../utils/compose";
+
+/*
+ * TextField Context
+ */
+interface TextFieldContext {
+  variant?: "primary" | "secondary";
+}
+
+const TextFieldContext = createContext<TextFieldContext>({});
+
+/*
+ * TextField Root
+ */
+interface TextFieldRootProps
+  extends ComponentPropsWithRef<typeof TextFieldPrimitive>, TextFieldVariants {
+  /**
+   * The variant of the text field.
+   * @default "primary"
+   */
+  variant?: "primary" | "secondary";
+}
+
+const TextFieldRoot = ({
+  children,
+  className,
+  fullWidth,
+  variant,
+  ...props
+}: TextFieldRootProps) => {
+  const styles = React.useMemo(
+    () => textFieldVariants({ fullWidth }),
+    [fullWidth]
+  );
+
+  return (
+    <TextFieldPrimitive
+      data-slot="textfield"
+      {...props}
+      className={composeTwRenderProps(className, styles)}
+    >
+      {(values) => (
+        <TextFieldContext value={{ variant }}>
+          <>{typeof children === "function" ? children(values) : children}</>
+        </TextFieldContext>
+      )}
+    </TextFieldPrimitive>
+  );
+};
+
+/*
+ * Exports
+ */
+export { TextFieldContext, TextFieldRoot };
+
+export type { TextFieldRootProps };
