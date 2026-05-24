eafa9822d0116888d7a9c1f265621b2f90ddee94
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-05-24 18:20:53 +0200

    refactor button component

--- packages/ui/src/components/button/button.tsx
+++ packages/ui/src/components/button/button.tsx
@@ -2,49 +2,40 @@
 
 import type { ButtonVariants } from "@lite-app/styles/components/button";
 import { buttonVariants } from "@lite-app/styles/components/button";
-import type { ComponentPropsWithRef } from "react";
-import { Button as ButtonPrimitive } from "react-aria-components/Button";
+import type { ButtonProps as RACButtonProps } from "react-aria-components/Button";
+import { Button as RACButton } from "react-aria-components/Button";
 
 import { cnRenderProps } from "../../utils/cn-render-props";
 
-interface ButtonRootProps
-  extends ComponentPropsWithRef<typeof ButtonPrimitive>, ButtonVariants {}
+interface ButtonProps extends RACButtonProps, ButtonVariants {}
 
-function ButtonRoot({
+function Button({
   variant,
   size,
-  isDisabled,
   fullWidth,
   isIconOnly,
-  slot,
   className,
-  style,
   children,
   ...rest
-}: ButtonRootProps) {
-  const styles = buttonVariants({
-    fullWidth,
-    isIconOnly,
-    size,
-    variant,
-  });
-
+}: ButtonProps) {
   return (
-    <ButtonPrimitive
+    <RACButton
       data-slot="button"
-      isDisabled={isDisabled}
-      slot={slot}
-      className={cnRenderProps(className, styles)}
-      style={style}
+      className={cnRenderProps(
+        className,
+        buttonVariants({
+          fullWidth,
+          isIconOnly,
+          size,
+          variant,
+        })
+      )}
       {...rest}
     >
-      {(renderProps) =>
-        typeof children === "function" ? children(renderProps) : children
-      }
-    </ButtonPrimitive>
+      {children}
+    </RACButton>
   );
 }
 
-export { ButtonRoot };
-
-export type { ButtonRootProps };
+export { Button };
+export type { ButtonProps };
--- packages/ui/src/components/button/index.ts
+++ packages/ui/src/components/button/index.ts
@@ -1,20 +1,5 @@
-import type { ComponentProps } from "react";
-
-import { ButtonRoot } from "./button";
-
-export const Button = Object.assign(ButtonRoot, {
-  Root: ButtonRoot,
-});
-
-export interface Button {
-  Props: ComponentProps<typeof ButtonRoot>;
-  RootProps: ComponentProps<typeof ButtonRoot>;
-}
-
-export { ButtonRoot };
-
-export type { ButtonRootProps as ButtonProps, ButtonRootProps } from "./button";
+export { Button } from "./button";
+export type { ButtonProps } from "./button";
 
 export { buttonVariants } from "@lite-app/styles/components/button";
-
 export type { ButtonVariants } from "@lite-app/styles/components/button";
