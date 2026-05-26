8807e2a0fb55d7f2d2ba5a9fb1f1a1b6573ca34e
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-05-26 12:20:24 +0200

    add schema context in better auth

--- apps/dashboard/app/lib/auth/index.server.ts
+++ apps/dashboard/app/lib/auth/index.server.ts
@@ -3,16 +3,17 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { admin } from "better-auth/plugins";
 
 import { db } from "../db";
-import { user as userTable } from "../db/schema";
+import * as schema from "../db/schema";
 
 export async function hasExistingUser() {
-  const userCount = await db.$count(userTable);
+  const userCount = await db.$count(schema.user);
   return userCount > 0;
 }
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite",
+    schema,
   }),
   databaseHooks: {
     user: {
