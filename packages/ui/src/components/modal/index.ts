fe047730453279d0a89592bf30ea76bab3158723
michaltarasiuk <tarasiuk.michal03@gmail.com>
2026-05-27 20:34:34 +0200

    add model component

---
+++ packages/ui/src/components/modal/index.ts
@@ -0,0 +1,29 @@
+export {
+  Modal,
+  ModalBackdrop,
+  ModalBody,
+  ModalCloseTrigger,
+  ModalContainer,
+  ModalDialog,
+  ModalFooter,
+  ModalHeader,
+  ModalHeading,
+  ModalIcon,
+  ModalTrigger,
+} from "./modal";
+export { modalVariants } from "./modal.variants";
+
+export type {
+  ModalBackdropProps,
+  ModalBodyProps,
+  ModalCloseTriggerProps,
+  ModalContainerProps,
+  ModalDialogProps,
+  ModalFooterProps,
+  ModalHeaderProps,
+  ModalHeadingProps,
+  ModalIconProps,
+  ModalProps,
+  ModalTriggerProps,
+} from "./modal";
+export type { ModalVariants } from "./modal.variants";
---
+++ packages/ui/src/components/modal/modal.tsx
@@ -0,0 +1,275 @@
+"use client";
+
+import { createContext } from "@lite-app/shared/create-context";
+import type { ComponentProps } from "react";
+import {
+  Dialog as RACDialog,
+  DialogTrigger as RACDialogTrigger,
+  Heading as RACHeading,
+  type DialogProps as RACDialogProps,
+  type DialogTriggerProps as RACDialogTriggerProps,
+  type HeadingProps as RACHeadingProps,
+} from "react-aria-components/Dialog";
+import {
+  Modal as RACModal,
+  ModalOverlay as RACModalOverlay,
+  Pressable as RACPressable,
+  type ModalOverlayProps as RACModalOverlayProps,
+} from "react-aria-components/Modal";
+
+import { cnRenderProps } from "../../utils/cn-render-props";
+import { CloseButton, type CloseButtonProps } from "../close-button";
+import { modalVariants, type ModalVariants } from "./modal.variants";
+
+const slots = modalVariants();
+
+interface ModalContextValue extends ModalVariants {
+  placement?: "auto" | "top" | "center" | "bottom";
+}
+
+const [ModalContext, useModalContext] =
+  createContext<ModalContextValue>("ModalContext");
+
+interface ModalProps extends RACDialogTriggerProps, ModalContextValue {}
+
+function Modal({
+  variant,
+  size,
+  scroll,
+  placement = "auto",
+  children,
+  ...rest
+}: ModalProps) {
+  return (
+    <ModalContext
+      value={{
+        placement,
+        scroll,
+        size,
+        variant,
+      }}
+    >
+      <RACDialogTrigger data-slot="modal" {...rest}>
+        {children}
+      </RACDialogTrigger>
+    </ModalContext>
+  );
+}
+
+export { Modal };
+export type { ModalProps };
+
+interface ModalTriggerProps extends ComponentProps<"div"> {}
+
+function ModalTrigger({ children, className, ...rest }: ModalTriggerProps) {
+  return (
+    <RACPressable>
+      <div
+        data-slot="modal-trigger"
+        className={slots.trigger({
+          className,
+        })}
+        {...rest}
+      >
+        {children}
+      </div>
+    </RACPressable>
+  );
+}
+
+export { ModalTrigger };
+export type { ModalTriggerProps };
+
+interface ModalBackdropProps extends RACModalOverlayProps {
+  isDismissable?: boolean;
+  isKeyboardDismissDisabled?: boolean;
+}
+
+function ModalBackdrop({
+  children,
+  className,
+  onClick,
+  ...rest
+}: ModalBackdropProps) {
+  const { variant } = useModalContext();
+  return (
+    <RACModalOverlay
+      data-slot="modal-backdrop"
+      className={cnRenderProps(
+        className,
+        slots.backdrop({
+          variant,
+        })
+      )}
+      onClick={(event) => {
+        event.stopPropagation();
+        onClick?.(event);
+      }}
+      {...rest}
+    >
+      {children}
+    </RACModalOverlay>
+  );
+}
+
+export { ModalBackdrop };
+export type { ModalBackdropProps };
+
+interface ModalContainerProps extends RACModalOverlayProps {}
+
+function ModalContainer({ children, className, ...rest }: ModalContainerProps) {
+  const { placement, scroll, size } = useModalContext();
+  return (
+    <RACModal
+      data-slot="modal-container"
+      data-placement={placement}
+      className={cnRenderProps(
+        className,
+        slots.container({
+          scroll,
+          size,
+        })
+      )}
+      {...rest}
+    >
+      {children}
+    </RACModal>
+  );
+}
+
+export { ModalContainer };
+export type { ModalContainerProps };
+
+interface ModalDialogProps extends RACDialogProps {}
+
+function ModalDialog({ children, className, ...rest }: ModalDialogProps) {
+  const { size, scroll, placement } = useModalContext();
+  return (
+    <RACDialog
+      data-slot="modal-dialog"
+      data-placement={placement}
+      className={slots.dialog({
+        className,
+        scroll,
+        size,
+      })}
+      {...rest}
+    >
+      {children}
+    </RACDialog>
+  );
+}
+
+export { ModalDialog };
+export type { ModalDialogProps };
+
+interface ModalHeaderProps extends ComponentProps<"div"> {}
+
+function ModalHeader({ children, className, ...rest }: ModalHeaderProps) {
+  return (
+    <div
+      data-slot="modal-header"
+      className={slots.header({
+        className,
+      })}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
+
+export { ModalHeader };
+export type { ModalHeaderProps };
+
+interface ModalHeadingProps extends RACHeadingProps {}
+
+function ModalHeading({ children, className, ...rest }: ModalHeadingProps) {
+  return (
+    <RACHeading
+      slot="title"
+      data-slot="modal-heading"
+      className={slots.heading({
+        className,
+      })}
+      {...rest}
+    >
+      {children}
+    </RACHeading>
+  );
+}
+
+export { ModalHeading };
+export type { ModalHeadingProps };
+
+interface ModalBodyProps extends ComponentProps<"div"> {}
+
+function ModalBody({ children, className, ...rest }: ModalBodyProps) {
+  const { scroll } = useModalContext();
+  return (
+    <div
+      data-slot="modal-body"
+      className={slots.body({
+        className,
+        scroll,
+      })}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
+
+export { ModalBody };
+export type { ModalBodyProps };
+
+interface ModalFooterProps extends ComponentProps<"div"> {}
+
+function ModalFooter({ children, className, ...rest }: ModalFooterProps) {
+  return (
+    <div
+      data-slot="modal-footer"
+      className={slots.footer({
+        className,
+      })}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
+
+export { ModalFooter };
+export type { ModalFooterProps };
+
+interface ModalIconProps extends ComponentProps<"div"> {}
+
+function ModalIcon({ children, className, ...rest }: ModalIconProps) {
+  return (
+    <div
+      data-slot="modal-icon"
+      className={slots.icon({
+        className,
+      })}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
+
+export { ModalIcon };
+export type { ModalIconProps };
+
+interface ModalCloseTriggerProps extends CloseButtonProps {}
+
+function ModalCloseTrigger({ className, ...rest }: ModalCloseTriggerProps) {
+  return (
+    <div data-slot="modal-close-trigger" className={slots.closeTrigger()}>
+      <CloseButton slot="close" className={className} {...rest} />
+    </div>
+  );
+}
+
+export { ModalCloseTrigger };
+export type { ModalCloseTriggerProps };
